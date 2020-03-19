cf5044813112614936219b390bf899e866e901ac
ziyan <46317855+ziyanfadilla@users.noreply.github.com>
2020-03-19 16:19:08 +0700

    Add files via upload

---
+++ Oop/src/oop/Aplikasi.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Aplikasi {public static void main(String[] args) {
+      String nama = "Ziyan Fadilla Saporette ";
+      String kelas = "4B";
+      String nim = "18090058";
+        System.out.println("Nama : "+nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Nim : "+nim);
+        
+    }
+     
+    
+}
